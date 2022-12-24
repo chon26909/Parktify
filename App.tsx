1c8992cc32bac9b41345e940c3cce3421fb32bcc
Chonlasit <chonlasit.s@arise.tech>
2022-12-24 22:22:46 +0700

    update google map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+
+import i from "./assets/favicon.png";
 
 export default function App() {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 13.621964154078512,
+    longitude: 100.53999614273243,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        region={mapRegion}
+      >
+        <Marker coordinate={mapRegion} />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
